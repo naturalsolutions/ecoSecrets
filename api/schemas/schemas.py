deedf1ab391c0610808adddc45f324a756d53520
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2022-07-13 11:09:36 +0200

    feat: add site, project, deployment, device

--- api/schemas/schemas.py
+++ api/schemas/schemas.py
@@ -1,7 +1,7 @@
-from typing import List, Union
+from typing import List, Union, Optional
 
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, date
 
 class ItemBase(BaseModel):
     title: str
@@ -53,4 +53,56 @@ class File(BaseModel):
     bucket: str
     date: datetime
     url: str
-    
+
+
+####################  PROJECT  ###############################
+##############################################################
+
+class Project(BaseModel):
+    name: str 
+    description: str
+    creation_date: Optional[datetime]
+    end_date: Optional[datetime]
+    status: Optional[str]
+    owner_id : int
+    contact_id : int
+
+####################  DEPLOYMENT  ############################
+##############################################################
+
+class Deployment(BaseModel):
+    id : Optional[int]
+    name: str
+    start_date : Optional[datetime]
+    end_date : Optional[datetime]
+    site_id : Optional[int]
+    device_id : Optional[int]
+    bait: str
+    feature: str
+    description: str
+    project_id : int
+    template_sequence_id :Optional[int]
+
+####################  SITE  ###############################
+###########################################################
+
+class Site(BaseModel):
+    id: int
+    name: str
+    habitat: str
+    description: str
+
+
+####################  DEVICE  ###############################
+###########################################################
+
+class Device (BaseModel):
+    id: int 
+    name: str
+    model:str
+    purchase_date: date
+    price: float
+    description: str
+    detection_area: float
+    status : str
+    exif_id: Optional[int]
