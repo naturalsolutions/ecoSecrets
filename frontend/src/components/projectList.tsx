68aa126e0d2d664de75abad775b4347ca3b66f5b
ophdlv <ophelie_da-silva@natural-solutions.eu>
2022-07-18 18:19:49 +0200

    feat: add icon buttons in cards

--- frontend/src/components/projectList.tsx
+++ frontend/src/components/projectList.tsx
@@ -1,6 +1,6 @@
 import { Card, CardMedia, CardContent, CardHeader, Divider, Grid, CardActions, IconButton } from "@mui/material";
-// import DownloadIcon from "@mui/icons-material/Download";
-// import VisibilityIcon from "@mui/icons-material/Visibility";
+import DownloadIcon from "@mui/icons-material/Download";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import { Link } from "react-router-dom";
 import { useMainContext } from "../contexts/mainContext";
 
@@ -24,15 +24,21 @@ const ProjectList = () => {
           <CardMedia
             component="img"
             height="194"
-            image="myimage.jpg"
+            image="https://cdn.pixabay.com/photo/2022/06/25/23/41/ladybug-7284337_960_720.jpg"
           />
           <CardContent>{p.description || "No description"}</CardContent>
           <CardActions disableSpacing>
-            <IconButton aria-label="Import data">
-              {/* <DownloadIcon /> */}
+            <IconButton 
+              aria-label="Import data" 
+            >
+              <DownloadIcon />
             </IconButton>
-            <IconButton aria-label="Inspect project">
-              {/* <VisibilityIcon /> */}
+            <IconButton 
+              aria-label="Inspect project"
+              component={Link}
+              to={`/project/${p.name}`}
+            >
+              <VisibilityIcon />
             </IconButton>
           </CardActions>
         </Card>
