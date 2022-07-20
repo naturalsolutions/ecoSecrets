b1ea260baa01ad23feed329adb277abbc479f0ee
Julien-Gr4z <julien_graziani@natural-solutions.eu>
2022-07-20 14:00:33 +0200

    fix: remove useless context

--- frontend/src/pages/deployment.tsx
+++ frontend/src/pages/deployment.tsx
@@ -2,19 +2,17 @@ import First from "../components/imageList";
 import MainLayout from "../layouts/mainLayout";
 import HeadBar from "../components/HeadBar";
 import Drawer from "../components/drawer";
-import MainContextProvider from "../contexts/mainContext";
 import NavigationPath from "../components/breadcrumb";
 
 function Deployment() {
   return (
     <div className="Main">
-      <MainContextProvider>
-        <MainLayout 
-          Header={<HeadBar />} 
-          Side={<Drawer />} 
-          Navigation={<NavigationPath />}
-          Main={<First />} />
-      </MainContextProvider>
+      <MainLayout
+        Header={<HeadBar />}
+        Side={<Drawer />}
+        Navigation={<NavigationPath />}
+        Main={<First />}
+      />
     </div>
   );
 }
