de72984c80f247c7a8913d387b162cccd97bb103
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2022-08-18 14:11:39 +0200

    wip: add route menu devices

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainContextProvider from "./contexts/mainContext";
 import NewDeployment from "./pages/newdeployment";
 import ProjectSheet from "./pages/projectSheet";
+import DeviceMenuPage from "./pages/deviceMenu";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/project/:projectId" element={<Project />}></Route>
+          <Route path="/devices/" element={<DeviceMenuPage />}></Route>
           <Route path="/deployment/new" element={<NewDeployment />}></Route>
           <Route path="/projectsheet/:projectId" element={<ProjectSheet />}></Route>
           <Route
