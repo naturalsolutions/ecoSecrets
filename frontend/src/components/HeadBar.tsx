09242b59e6c1f072d4ca07ac1b2455ead4e36567
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-02-20 16:28:43 +0100

    feat(front): add logout button to headbar

--- frontend/src/components/HeadBar.tsx
+++ frontend/src/components/HeadBar.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -10,28 +11,35 @@ import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import ImportModale from "./importModale";
 import { Link } from "react-router-dom";
 import ModalError from "./modalError";
-import { Button, capitalize } from "@mui/material";
+import { Button, capitalize, Menu, MenuItem } from "@mui/material";
 import LanguageSelector from "./languageSelector";
 import { useTranslation } from "react-i18next";
+import { AuthContext } from "../contexts/AuthContextProvider";
 
 const HeadBar = () => {
   const { t } = useTranslation();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [openImport, setOpenImport] = useState(false);
-  const [open, setOpen] = useState(false);
-
+  const open = Boolean(anchorEl);
+  const { logout } = useContext(AuthContext);
   const openImportModale = () => {
     setOpenImport(true);
   };
 
-  const handleClickOpen = () => {
-    open ? setOpen(false) : setOpen(true)
-    setTimeout(() => {
-      setOpen(false)
-    }, 100)
-  };
-
   const closeImportModale = () => {
     setOpenImport(false);
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    logout();
+    handleClose();
   };
 
   return (
@@ -44,10 +52,10 @@ const HeadBar = () => {
           justifyContent="flex-start"
           alignItems="center"
         >
-          <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={handleClickOpen}>
+          <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <VideoCameraBackIcon />
           </IconButton>
-          <Link to={`/`} style={{ textDecoration: 'none' }}>
+          <Link to={`/`} style={{ textDecoration: "none" }}>
             <Typography
               variant="h6"
               noWrap
@@ -87,12 +95,28 @@ const HeadBar = () => {
 
           <LanguageSelector />
 
-          <IconButton aria-label="menu" sx={{ mr: 2, display: { color: "white" } }} onClick={handleClickOpen}>
-            < AccountCircleIcon/>
-          </IconButton>
-          <ModalError open={open} />
+        <IconButton
+          aria-label="menu"
+          sx={{ mr: 2, display: { color: "white" } }}
+          onClick={handleClick}
+        >
+          <AccountCircleIcon />
+        </IconButton>
+        <ModalError open={open} />
+        <Menu
+          id="basic-menu"
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleLogout}>
+            {capitalize(t("main.logout"))}
+          </MenuItem>
+        </Menu>
         </Grid>
-        
       </Toolbar>
     </AppBar>
   );
