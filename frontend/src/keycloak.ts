0d3da72f46024f4b17eff85995ce0627d26fb9ae
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-05-27 23:24:17 +0200

    chore(frontend): remove console.log

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -2,7 +2,6 @@ import Keycloak from "keycloak-js";
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from
 // 'keycloak.json'
-console.log("yolo", process.env.REACT_APP_KEYCLOAK_CLIENT_REALM)
 const keycloak = new Keycloak({
   url: process.env.REACT_APP_KEYCLOAK_CLIENT_URL,
   realm: process.env.REACT_APP_KEYCLOAK_CLIENT_REALM || "",
