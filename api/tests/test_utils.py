ddc14839b3bcfe363db1afb3d4b8641798e6bb9b
ophdlv <ophelie_da-silva@natural-solutions.eu>
2023-09-04 15:39:38 +0200

    test: modify check mime test

--- api/tests/test_utils.py
+++ api/tests/test_utils.py
@@ -1,35 +1,41 @@
+import pytest
+
 from src.utils import check_mime
 
+all_authorized_mimes = [
+    "video/mp4",
+    "video/mpeg",
+    "video/ogg",
+    "video/webm",
+    "image/jpeg",
+    "image/png",
+    "image/bmp",
+    "image/gif",
+    "image/tiff",
+    "image/webp",
+]
 
-def test_check_mime():
-    authorized_mime = [
-        "video/mp4",
-        "video/mpeg",
-        "video/ogg",
-        "video/webm",
-        "image/jpeg",
-        "image/png",
-        "image/bmp",
-        "image/gif",
-        "image/tiff",
-        "image/webp",
-    ]
+some_unauthorized_mimes = [
+    "audio/aac",
+    "video/x-msvideo",
+    "application/pdf",
+    "application/xml",
+    "application/vnd.mozilla.xul+xml",
+    "application/zip",
+    "video/3gpp2",
+    "audio/3gpp2",
+    "application/x-7z-compressed",
+]
 
-    ex_unauthorized_mime = [
-        "audio/aac",
-        "video/x-msvideo",
-        "application/pdf",
-        "application/xml" "application/vnd.mozilla.xul+xml",
-        "application/zip",
-        "video/3gpp2",
-        "audio/3gpp2",
-        "application/x-7z-compressed",
-    ]
 
-    assert any(item in authorized_mime for item in ex_unauthorized_mime) == False
+if any(item in all_authorized_mimes for item in some_unauthorized_mimes):
+    raise Exception(
+        "Value(s) shared between lists of authorized and unauthorized mime types. Tests cannot be performed."
+    )
 
-    for item in authorized_mime:
-        assert check_mime(item)
 
-    for item in ex_unauthorized_mime:
-        assert check_mime(item) == False
+@pytest.mark.parametrize("authorized", all_authorized_mimes)
+@pytest.mark.parametrize("unauthorized", some_unauthorized_mimes)
+def test_check_mime(authorized, unauthorized):
+    assert check_mime(authorized)
+    assert not check_mime(unauthorized)
