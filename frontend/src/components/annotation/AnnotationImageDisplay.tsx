39cc1f07e7f85b725af253321273b0270664f754
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2023-11-30 23:55:33 +0100

    fix: annotation buttons display on small sreen

--- frontend/src/components/annotation/AnnotationImageDisplay.tsx
+++ frontend/src/components/annotation/AnnotationImageDisplay.tsx
@@ -2,75 +2,77 @@ import { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
 import { useMainContext } from "../../contexts/mainContext";
 import { useAnnotationContext } from "../../contexts/annotationContext";
+import { height } from "@mui/system";
+import { isPropertyAccessChain } from "typescript";
 
 const AnnotationImageDisplay = () => {
+  const [isAnnotatedColor, setIsAnnotatedColor] = useState<string>("");
+  const { image } = useMainContext();
+  const { annotated } = useAnnotationContext();
 
-    const [isAnnotatedColor, setIsAnnotatedColor] = useState<string>("");
-    const { image } = useMainContext();
-    const { annotated } = useAnnotationContext();
+  const mediaDisplayStyle = {
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+    width: "100%",
+    height: "100%",
+    "object-fit": "contain",
+    display: "block",
+    // border: "3px solid",
+    // borderRadius: "5px",
+    // borderColor: isAnnotatedColor,
+  };
 
-    const mediaDisplayStyle = {
-        borderBottomLeftRadius: 4,
-        borderBottomRightRadius: 4,
-        maxWidth: "99.5%",
-        maxHeight: "670px",
-        border: "3px solid",
-        borderRadius: "5px",
-        borderColor: isAnnotatedColor,
-    };
-    
-    const displayMedia = (image) => {
-        if (image.extension.includes("image")) {
-            return (
-                <img
-                    src={ image.url }
-                    alt={ image.name }
-                    loading="lazy"
-                    style={ mediaDisplayStyle }
-                />
-            )
-        }
-        else {
-            return (
-                <video
-                    src={ image.url }
-                    style={ mediaDisplayStyle }
-                    controls
-                    autoPlay={ false }
-                >
-                    <source 
-                        type="video/mp4"
-                    />
-                    { image.name }
-                </video>
-            )
-        }
-    };
+  const displayMedia = (image) => {
+    if (image.extension.includes("image")) {
+      return (
+        <img
+          src={image.url}
+          alt={image.name}
+          loading="lazy"
+          style={mediaDisplayStyle}
+        />
+      );
+    } else {
+      return (
+        <video
+          src={image.url}
+          style={mediaDisplayStyle}
+          controls
+          autoPlay={false}
+        >
+          <source type="video/mp4" />
+          {image.name}
+        </video>
+      );
+    }
+  };
 
-    useEffect(() => {
-        (async () => {
-            image()?.treated
-            ? setIsAnnotatedColor("green") 
-            : (annotated 
-                ? setIsAnnotatedColor("orange") 
-                : setIsAnnotatedColor("red"))
-        })();
-    }, [image()?.annotations, annotated]);
+  useEffect(() => {
+    (async () => {
+      image()?.treated
+        ? setIsAnnotatedColor("green")
+        : annotated
+        ? setIsAnnotatedColor("orange")
+        : setIsAnnotatedColor("red");
+    })();
+  }, [image()?.annotations, annotated]);
 
-    return (
-        <>
-            <Grid item
-                alignItems="center"
-                justifyContent="center"
-                className="boxImage"
-                style={{ 
-                    backgroundColor: "#D9D9D9",
-                }}
-            >
-                { displayMedia(image()) }
-            </Grid>
-        </>
-    );
+  return (
+    <>
+      <Grid
+        item
+        alignItems="center"
+        justifyContent="center"
+        className="boxImage"
+        style={{
+          backgroundColor: "#D9D9D9",
+          overflow: "auto",
+        }}
+      >
+        {displayMedia(image())}
+      </Grid>
+    </>
+  );
 };
 
 export default AnnotationImageDisplay;
--- frontend/src/components/annotation/AnnotationMain.tsx
+++ frontend/src/components/annotation/AnnotationMain.tsx
@@ -4,22 +4,23 @@ import AnnotationSaveError from "./AnnotationSaveError";
 import "../../css/annotation.css";
 import AnnotationImage from "./AnnotationImage";
 
-
-
 const LayoutAnnotationContainer = styled("div")({
   flexGrow: 1,
   display: "grid",
   gridTemplateColumns: "repeat(12, 1fr)",
   columnGap: "1rem",
   rowGap: "1rem",
+  overflowY: "auto",
 });
 
 const LayoutAnnotationImage = styled("div")(({ theme }) => ({
   gridColumn: "1/8",
+  height: "100%",
+  overflow: "auto",
   [theme.breakpoints.down("md")]: {
     gridColumn: "1/13",
     gridRow: "1/5",
-  }
+  },
 }));
 
 const LayoutAnnotationForm = styled("div")(({ theme }) => ({
@@ -32,20 +33,18 @@ const LayoutAnnotationForm = styled("div")(({ theme }) => ({
 }));
 
 const AnnotationMain = () => {
-  
-    return (
-        <LayoutAnnotationContainer className="page">
+  return (
+    <LayoutAnnotationContainer className="page">
+      <LayoutAnnotationImage>
+        <AnnotationImage />
+      </LayoutAnnotationImage>
 
-            <LayoutAnnotationImage>
-                <AnnotationImage />
-            </LayoutAnnotationImage >
+      <LayoutAnnotationForm className="annotations">
+        <AnnotationForm />
+      </LayoutAnnotationForm>
 
-            <LayoutAnnotationForm className="annotations">
-                <AnnotationForm />
-            </LayoutAnnotationForm >
-
-            <AnnotationSaveError />
-        </LayoutAnnotationContainer>
-    );
+      <AnnotationSaveError />
+    </LayoutAnnotationContainer>
+  );
 };
 export default AnnotationMain;
