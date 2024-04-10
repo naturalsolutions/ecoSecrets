6c3c3161623e2156d8f89325048e980ad2607577
Killian Rakotonanahary <killian_rakotonanahary@natural-solutions.eu>
2024-04-10 18:39:27 +0200

    ajout de la carte

--- frontend/src/components/deviceMenu/deviceMenuMain.tsx
+++ frontend/src/components/deviceMenu/deviceMenuMain.tsx
@@ -7,13 +7,9 @@ import {
   Typography,
   capitalize,
 } from "@mui/material";
-import Filters from "./filters";
 import DevicesTable from "./devicesTable";
 import DeviceModal from "./deviceModal";
 import { useTranslation } from "react-i18next";
-import { useMainContext } from "../../contexts/mainContext";
-import { useEffect, useState } from "react";
-import Map from '../Map';
 
 const DeviceMenu = () => {
   const { t } = useTranslation();
@@ -40,15 +36,6 @@ const DeviceMenu = () => {
         </Typography>
         <DevicesTable />
       </Stack>
-      {/* {projectSheetData.deployments.length !== 0 && position.length !== 0 ?
-      <Grid container justifyContent="center" alignItems='center'>
-        <Grid container item justifyContent="center" height={400} width={1000} spacing={1} style={{ backgroundColor: "#D9D9D9" }}>
-          <Map position={position} zoom={2} />
-        </Grid>
-      </Grid>
-      :
-      <></>
-} */}
     </Stack>
   );
 };
