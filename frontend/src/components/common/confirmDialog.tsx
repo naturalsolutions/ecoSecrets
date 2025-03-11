187097a43adad4306c01fcd7c603a48b259b96ef
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2025-03-12 00:32:11 +0100

    oopsi: add missing filing in previous commit

---
+++ frontend/src/components/common/confirmDialog.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from "react-i18next";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+  Divider,
+} from "@mui/material";
+
+interface ConfirmDialogProps {
+  open: boolean;
+  title?: string;
+  message?: string;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+export default function ConfirmDialog({
+  open,
+  title = "Confirmation",
+  message = "Êtes-vous sûr de vouloir effectuer cette action ?",
+  onClose,
+  onConfirm,
+}: ConfirmDialogProps) {
+  const { t } = useTranslation();
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} variant="outlined" color="secondary">
+          {t("main.cancel")}
+        </Button>
+        <Button onClick={onConfirm} variant="contained" color="primary">
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
