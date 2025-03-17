a8a67d21e05fff0cf8a4634ee821f521f1418ccf
ophdlv <ophelie_da-silva@natural-solutions.eu>
2025-03-17 11:24:48 +0100

    feat: tooltip on gotomedia arrow in grid view

--- frontend/src/components/common/ButtonGoAnnotation.tsx
+++ frontend/src/components/common/ButtonGoAnnotation.tsx
@@ -1,9 +1,10 @@
-import { IconButton } from "@mui/material";
+import { capitalize, IconButton, Tooltip } from "@mui/material";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useNavigate } from "react-router-dom";
 import { useMainContext } from "../../contexts/mainContext";
 import { useAnnotationContext } from "../../contexts/annotationContext";
 import { useFilesContext } from "../../contexts/filesContext";
+import { t } from "i18next";
 
 const ButtonGoAnnotation = (
     props
@@ -21,13 +22,15 @@ const ButtonGoAnnotation = (
     };
 
     return(
-        <IconButton
-            sx={{ color: 'rgba(255, 255, 255, 0.7)' }}
-            aria-label={`info about ${ props.name }`}
-            onClick={ handleClickButton }
-        >
-            <ArrowForwardIcon />
-        </IconButton>
+        <Tooltip arrow title={capitalize(t("annotations.see"))}>
+            <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.7)' }}
+                aria-label={`info about ${ props.name }`}
+                onClick={ handleClickButton }
+            >
+                <ArrowForwardIcon />
+            </IconButton>
+        </Tooltip>
 )};
 
 export default ButtonGoAnnotation;
