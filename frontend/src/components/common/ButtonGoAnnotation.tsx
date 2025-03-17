f0f562c533b84dce3543ddc73b963ee3ddb67e91
ophdlv <ophelie_da-silva@natural-solutions.eu>
2025-03-17 22:37:28 +0100

    fix: project id in url

--- frontend/src/components/common/ButtonGoAnnotation.tsx
+++ frontend/src/components/common/ButtonGoAnnotation.tsx
@@ -1,6 +1,6 @@
 import { capitalize, IconButton, Tooltip } from "@mui/material";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useMainContext } from "../../contexts/mainContext";
 import { useAnnotationContext } from "../../contexts/annotationContext";
 import { useFilesContext } from "../../contexts/filesContext";
@@ -9,14 +9,14 @@ import { t } from "i18next";
 const ButtonGoAnnotation = (
     props
 ) => {
-    
+    const params = useParams();
     const navigate = useNavigate();
-    const { currentProject, currentDeployment } = useMainContext();
     const { setCurrentImage } = useFilesContext();
     const { setGridView } = useAnnotationContext();
     
+
     const handleClickButton = () => {
-        navigate(`/project/${ currentProject }/deployment/${ currentDeployment }/medias/${ props.id }`);
+        navigate(`/project/${ params.projectId }/deployment/${ params.deploymentId }/medias/${ props.id }`);
         setCurrentImage(props.id);
         setGridView(false);
     };
