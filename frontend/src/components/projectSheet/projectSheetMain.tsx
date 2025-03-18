1fc2ec9bf8d57e256f64753ee1569ad8ec3fa6e7
ophdlv <ophelie_da-silva@natural-solutions.eu>
2025-03-18 10:37:26 +0100

    fix: resolve warnings

--- frontend/src/components/projectSheet/projectSheetMain.tsx
+++ frontend/src/components/projectSheet/projectSheetMain.tsx
@@ -112,7 +112,7 @@ const ProjectSheet = () => {
         {capitalize(t("projects.sheet"))}
       </Typography>
       <Stack direction="row" alignItems="center" spacing={2}>
-        <Grid lg={2} xs={12}>
+        <Grid item lg={2} xs={12}>
           <ThumbnailProjectComponent
             modifyState={modifyState}
             setModifyState={setModifyState}
@@ -120,7 +120,7 @@ const ProjectSheet = () => {
             file={file}
           />
         </Grid>
-        <Grid lg={10} xs={12}>
+        <Grid item lg={10} xs={12}>
           <ProjectForm
             setModifyState={setModifyState}
             file={file}
