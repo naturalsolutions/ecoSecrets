b43be37a801e8a9aef5e4b7373ab0c9b71ce0008
manonbiboud <manon_biboud@natural-solutions.eu>
2025-08-04 13:53:07 +0200

    feat(mod_data_prep): add filter project, site, deployment

--- shiny/R/mod_data_prep.R
+++ shiny/R/mod_data_prep.R
@@ -1,8 +1,14 @@
 mod_data_prep_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    radioButtons(ns("project_select"), "Choix du projet :", choices = c("ecopont"), selected = "ecopont")
-  )
+    selectInput(ns("project_select"), "Choix du projet :", choices = c("ecopont"), selected = "ecopont"),
+    selectInput(ns("site_select"), "Choix du site :", choices = c("Montagny-les-Lanches", "Porte de Savoie", "Torchefelon"), selected = "Montagny-les-Lanches"),
+    selectInput(ns("deployment_select"), "Choix du déploiement :", choices = c("MLL_1_2023", "MLL_2_2023", "MLL_3_2023", "MLL_4_2023")),
+    checkboxInput(ns("empty_select"),"Retirer les élements vides", value = FALSE),
+    checkboxInput(ns("undetermined_select"),"Retirer les élements indéterminés", value = FALSE),
+    checkboxInput(ns("human_select"),"Retirer les humains", value = FALSE),
+    checkboxInput(ns("vehicle_select"),"Retirer les véhicules", value = FALSE)
+    )
 }
 
 mod_data_prep_server <- function(id) {
@@ -10,8 +16,8 @@ mod_data_prep_server <- function(id) {
     data <- reactive({
       set.seed(1234)
       df <- dataset
-      req(input$project_select)
-      dplyr::filter(df, project == input$project_select)
+      req(input$project_select, input$site_select, input$deployment_select)
+      dplyr::filter(df, project == input$project_select, site == input$site_select, deployment == input$deployment_select)
     })
     return(data)
   })
