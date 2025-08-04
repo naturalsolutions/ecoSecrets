7440dffac21795792928a3fb587b73240e9106c5
manonbiboud <manon_biboud@natural-solutions.eu>
2025-08-04 13:58:30 +0200

    feat(mod_data_prep): add choices in select link to dataset

--- shiny/R/mod_data_prep.R
+++ shiny/R/mod_data_prep.R
@@ -1,9 +1,9 @@
 mod_data_prep_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    selectInput(ns("project_select"), "Choix du projet :", choices = c("ecopont"), selected = "ecopont"),
-    selectInput(ns("site_select"), "Choix du site :", choices = c("Montagny-les-Lanches", "Porte de Savoie", "Torchefelon"), selected = "Montagny-les-Lanches"),
-    selectInput(ns("deployment_select"), "Choix du déploiement :", choices = c("MLL_1_2023", "MLL_2_2023", "MLL_3_2023", "MLL_4_2023")),
+    selectInput(ns("project_select"), "Choix du projet :", choices = unique(dataset$project), selected = "ecopont"),
+    selectInput(ns("site_select"), "Choix du site :", choices = unique(dataset$site)),
+    selectInput(ns("deployment_select"), "Choix du déploiement :", choices = unique(dataset$deployment)),
     checkboxInput(ns("empty_select"),"Retirer les élements vides", value = FALSE),
     checkboxInput(ns("undetermined_select"),"Retirer les élements indéterminés", value = FALSE),
     checkboxInput(ns("human_select"),"Retirer les humains", value = FALSE),
