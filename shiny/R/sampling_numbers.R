78a02375185ccc61d8134dd2b3a48705b0a8d3c6
manonbiboud <manon_biboud@natural-solutions.eu>
2025-08-06 14:59:07 +0200

    feat: add EP per species function

--- shiny/R/sampling_numbers.R
+++ shiny/R/sampling_numbers.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(lubridate)
 library(dplyr)
 
-#================== NOMBRE EVENEMENT PHOTOGRAPHIQUE=============================
+#================== NOMBRE EVENEMENT PHOTOGRAPHIQUE GLOBAL =============================
 
 nb_EP <- function(df, interval_ind=5) {
   
@@ -18,42 +18,46 @@ nb_EP <- function(df, interval_ind=5) {
     pull(total_evenements)
 }
 
+#================== NOMBRE EVENEMENT PHOTOGRAPHIQUE PAR ESPECE =============================
+
+nb_EP_par_espece <- function(df, interval_ind = 5) {
+  df %>%
+    mutate(
+      species = ifelse(is.na(species) | species == "", "Indéterminé", species)
+    ) %>%
+    filter(!is.na(date)) %>%  # On garde quand même des dates valides
+    arrange(species, date) %>%
+    group_by(species) %>%
+    filter(n() > 0) %>%
+    mutate(
+      temps_ecoule = as.numeric(difftime(date, lag(date), units = "mins")),
+      nouvel_evenement = is.na(temps_ecoule) | temps_ecoule > interval_ind
+    ) %>%
+    summarise(nb_evenements = sum(nouvel_evenement), .groups = "drop")
+}
+
 #================== JOURS THEORIQUES ====================================
 
-nb_trap_days_estimated <- function(df, deployment, start_date, end_date) {
-  
-  #Filtre déploiement
-  if (!is.null(deployment)) {
-    df <- df %>% filter(ecopont == deployment)
-  }
+nb_trap_days_estimated <- function(df, start_date, end_date) {
   
   # Prétraitement des dates
   df <- df %>%
-    mutate(date = as.POSIXct(date, format = "%d/%m/%Y %H:%M"),
-           date = as.Date(date))
-  
-  #Filtre date
-  df <- df %>% filter(date >= start_date, date <= end_date)
+    mutate(date = as.Date(date)) %>%
+    filter(date >= start_date, date <= end_date)
   
   # Calcul de la différence de jours
   diff <- as.numeric(difftime(max(df$date, na.rm = TRUE), min(df$date, na.rm = TRUE), units = "days")) + 1
   
-  return(diff)
+  print(diff)
 }
 
 #================== JOURS REELS ====================================
 
-nb_trap_days_real <- function(df, deployment, start_date, end_date) {
+nb_trap_days_real <- function(df, start_date, end_date) {
 
-  #Filtre déploiement
-  if (!is.null(deployment)) {
-    df <- df %>% filter(ecopont == deployment)
-  }
-  
   #Filtre date
   df <- df %>%
-    mutate(date = as.POSIXct(date, format = "%d/%m/%Y %H:%M"),
-           date = as.Date(date)) %>%
+    mutate(date = as.Date(date)) %>%
     filter(date >= start_date, date <= end_date)
   
   df <- df[!is.na(df$date), ]
