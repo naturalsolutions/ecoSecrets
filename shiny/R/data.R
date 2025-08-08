1e9fc91267e441fce248ead93fca5280dc137fbf
manonbiboud <manon_biboud@natural-solutions.eu>
2025-08-08 16:26:21 +0200

    fix: change date format

--- shiny/R/data.R
+++ shiny/R/data.R
@@ -1,1 +1,3 @@
-dataset <- read.csv2("R/data/data.csv")
+dataset <- read.csv2("R/data/data.csv") %>% 
+  mutate(date = as.POSIXct(date, format = "%Y-%m-%d %H:%M:%S"),
+         date = if_else(year(date) == 2025 & device == "PDS_1", update(date, year = 2024), date))
