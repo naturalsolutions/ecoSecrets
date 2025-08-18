afdab99387ea9a8e93157c37decdfbc548079f14
manonbiboud <manon_biboud@natural-solutions.eu>
2025-08-18 11:53:24 +0200

    fix : data select input with min and max

--- shiny/R/mod_display_params.R
+++ shiny/R/mod_display_params.R
@@ -19,6 +19,7 @@ mod_display_params_server <- function(id, analysis_type, selected_analysis, data
           selectInput(ns("species_select"), "Choisir une espèce :", choices = unique(dataset$species), selected = NULL),
           numericInput(ns("interval_ind"), "Choisir un intervalle d'indépendance :", value = 0, min = 1, max = 300, step = 5)
         )
+        
       } else if (analysis_type() == "community") {
         if (selected_analysis() == "Abondance") {
           tagList(
@@ -30,18 +31,21 @@ mod_display_params_server <- function(id, analysis_type, selected_analysis, data
             shinyWidgets::switchInput(ns("show_group"), label = "Affichage", onLabel = "Empilé", offLabel = "Groupé", value = TRUE),
             radioButtons(ns("show_by"), "Affichage par :", choices = c("Projet" = "project", "Site" = "site", "Déploiement" = "deployment"), selected = "project")
           )
+          
         } else if (selected_analysis() == "Indice de diversité") {
           tagList(
-            dateRangeInput(ns("period_select"), "Choisir une période d'étude :", startview = "month"),
+            dateRangeInput(ns("period_select"), "Choisir une période d'étude :", start = min(as.Date(data()$date), na.rm = TRUE), end = max(as.Date(data()$date), na.rm = TRUE), startview = "month"),
             selectInput(ns("taxon_select"), "Choisir la résolution taxonomique :", choices = c("class", "order", "genus", "family", "species"), selected = "genus"),
             selectInput(ns("index_select"), "Choisir l'indice de richesse :", choices = c("shannon", "simpson"), selected = "shannon")
           )
         }
+        
       } else if (analysis_type() == "species") {
         tagList(
-          dateRangeInput(ns("period_select"), "Choisir une période d'étude :", startview = "month"),
+          dateRangeInput(ns("period_select"), "Choisir une période d'étude :", start = min(as.Date(data()$date), na.rm = TRUE), end = max(as.Date(data()$date), na.rm = TRUE), startview = "month"),
           selectizeInput(ns("species_select"), "Choisir une espèce :", choices = unique(dataset$species), selected = unique(dataset$species), multiple = TRUE),
-          numericInput(ns("interval_ind"), "Choisir un intervalle d'indépendance :", value = 0, min = 1, max = 300, step = 5)
+          numericInput(ns("interval_ind"), "Choisir un intervalle d'indépendance :", value = 0, min = 1, max = 300, step = 5),
+          radioButtons(ns("show_by"), "Affichage par :", choices = c("Projet" = "project", "Site" = "site", "Déploiement" = "deployment"), selected = "project")
         )
       }
     })
@@ -90,7 +94,8 @@ mod_display_params_server <- function(id, analysis_type, selected_analysis, data
             start_date = input$period_select[1] %||% min(as.Date(df$date), na.rm = TRUE),
             end_date = input$period_select[2] %||% max(as.Date(df$date), na.rm = TRUE),
             species = input$species_select,
-            interval_ind = input$interval_ind %||% 1
+            interval_ind = input$interval_ind %||% 1,
+            show_by = input$show_by %||% "project"
           )
         }
         else if (selected_analysis() == "species YYY") {
